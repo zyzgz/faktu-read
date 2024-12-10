2f01cdec4460166729f44ef714f45733da922a0f
Mateusz <mateusz.obloza.0@gmail.com>
2024-12-10 14:40:05 +0100

    feat(dashboard): add dashboard view WIP

--- client/src/app/features/dashboard/dashboard.component.ts
+++ client/src/app/features/dashboard/dashboard.component.ts
@@ -1,9 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { Avatar } from 'primeng/avatar';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [Avatar],
   templateUrl: './dashboard.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
