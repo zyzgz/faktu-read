18e40c3e83148cdf27b7d673327d04b3e79d1e47
Mateusz <mateusz.obloza.0@gmail.com>
2024-12-10 09:43:12 +0100

    chore: create base architecture for the app

---
+++ client/src/app/features/reports/generate/generate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate',
+  standalone: true,
+  imports: [],
+  templateUrl: './generate.component.html',
+  styleUrl: './generate.component.scss'
+})
+export class GenerateComponent {
+
+}
