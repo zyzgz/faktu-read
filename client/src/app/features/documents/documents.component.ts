7da442dbeb841c4c0943cc28019f58da66844d48
Mateusz <mateusz.obloza.0@gmail.com>
2024-12-13 19:39:29 +0100

    feat(documents): add loading spinner and block UI during invoice upload

--- client/src/app/features/documents/documents.component.ts
+++ client/src/app/features/documents/documents.component.ts
@@ -1,5 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { FileUpload, FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
+import { ChangeDetectionStrategy, Component, inject, signal, ViewChild } from '@angular/core';
+import {
+  FileProgressEvent,
+  FileUpload,
+  FileUploadHandlerEvent,
+  FileUploadModule,
+} from 'primeng/fileupload';
 import { DocumentsService } from './documents.service';
 import { MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
@@ -7,11 +12,22 @@ import { TableModule } from 'primeng/table';
 import { Toast } from 'primeng/toast';
 import { Invoice } from './interfaces/invoice.interface';
 import { CurrencyPipe } from '@angular/common';
+import { ProgressSpinner } from 'primeng/progressspinner';
+import { BlockUI } from 'primeng/blockui';
 
 @Component({
   selector: 'app-documents',
   standalone: true,
-  imports: [FileUpload, FileUploadModule, TableModule, ButtonModule, Toast, CurrencyPipe],
+  imports: [
+    FileUpload,
+    FileUploadModule,
+    TableModule,
+    ButtonModule,
+    Toast,
+    CurrencyPipe,
+    ProgressSpinner,
+    BlockUI,
+  ],
   providers: [MessageService],
   templateUrl: './documents.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,14 +36,16 @@ export class DocumentsComponent {
   private readonly documents = inject(DocumentsService);
   private readonly message = inject(MessageService);
 
+  @ViewChild(FileUpload) fileUpload!: FileUpload;
+
   uploadedInvoices: Invoice[] = [];
+  isLoading = signal(false);
 
   uploadInvoices(event: FileUploadHandlerEvent): void {
+    this.isLoading.set(true);
     const files: File[] = Array.from(event.files);
     this.documents.uploadInvoices(files).subscribe({
       next: (response) => {
-        console.log(response);
-
         this.uploadedInvoices = response.results.map((result) => result.invoices).flat();
         const successCount = response.results.filter((result) => result.message).length;
 
@@ -38,6 +56,7 @@ export class DocumentsComponent {
             detail: `${successCount} faktur przesłano pomyślnie.`,
           });
         }
+        this.fileUpload.clear();
       },
       error: () => {
         this.message.add({
@@ -46,6 +65,7 @@ export class DocumentsComponent {
           detail: 'Przesyłanie nie powiodło się, spróbuj ponownie.',
         });
       },
+      complete: () => this.isLoading.set(false),
     });
   }
 }
